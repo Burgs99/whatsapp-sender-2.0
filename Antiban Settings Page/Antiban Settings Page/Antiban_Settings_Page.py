7c5e322e34c14cb79afa13955b7626addf34c2b4
Dean <deangijohn@gmail.com>
2026-08-31 17:27:57 +0100

    Adding notations to Antiban_Settings_Page.py

--- Antiban Settings Page/Antiban Settings Page/Antiban_Settings_Page.py
+++ Antiban Settings Page/Antiban Settings Page/Antiban_Settings_Page.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from AntibanSettingsGUI import AntibanSettingsGUI
 from AntibanSettingsController import AntibanSettingsController
 
-
+# Starting point for the Antiban settings page, builds the screen and connects it to the database logic
 def main():
     root = tk.Tk()
 
@@ -14,6 +14,6 @@ def main():
 
     root.mainloop()
 
-
+# Only runs main() if this file is opened directly
 if __name__ == "__main__":
     main()
