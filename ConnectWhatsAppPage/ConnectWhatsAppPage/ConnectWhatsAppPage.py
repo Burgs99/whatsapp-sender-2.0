f902f5221e2e659248fbb902fba7d2e2ef21707b
Dean <deangijohn@gmail.com>
2026-08-31 19:59:25 +0100

    Adding notations to ConnectWhatsAppPage.py
    Added a comment to indicate the starting point of the application.

--- ConnectWhatsAppPage/ConnectWhatsAppPage/ConnectWhatsAppPage.py
+++ ConnectWhatsAppPage/ConnectWhatsAppPage/ConnectWhatsAppPage.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from connect_whatsapp_gui import ConnectWhatsAppGUI
 
-
+# Starting point for the Connect WhatsApp page, opens window
 if __name__ == "__main__":
     root = tk.Tk()
     app = ConnectWhatsAppGUI(root)
