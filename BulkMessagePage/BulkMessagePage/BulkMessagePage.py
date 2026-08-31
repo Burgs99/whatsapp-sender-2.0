86cb467a32d664c165068300a1dc1e055551f893
Dean <deangijohn@gmail.com>
2026-08-31 17:59:21 +0100

    Adding notation to the BulkMessagePage.py
    Added a comment to indicate the starting point of the Bulk Message Page.

--- BulkMessagePage/BulkMessagePage/BulkMessagePage.py
+++ BulkMessagePage/BulkMessagePage/BulkMessagePage.py
@@ -1,6 +1,6 @@
 from BulkMessageGUI import BulkMessageGUI
 
-
+#Starting Point for the Bulk Message Page, creates and opens the window
 if __name__ == "__main__":
     app = BulkMessageGUI()
     app.run()
